85ec4d5a14302a3230b66ead9973f283098a7c13
Callin Mullaney <cmullaney@augustash.com>
2014-04-04 10:49:44 -0500

    first commit of initial build

---
+++ templates/easy_admin.tpl.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * @file
+ * Default template for admin easy_admin.
+ *
+ */
+?>
+<div class="container">
+<ul id="gn-menu" class="gn-menu-main">
+
+	<li class="gn-trigger">
+	
+		<a class="gn-icon gn-icon-menu"><span>Menu</span></a>
+		
+		<nav id="easy_admin" class="<?php print $classes; ?> gn-menu-wrapper clearfix">
+			<div class="gn-scroller">
+				<ul class="gn-menu">
+    				<li><i class="fa fa-coffee"></i>test</li>
+    				<li><?php print render($easy_admin['easy_admin_menu']); ?></li>
+    			</ul>
+    		</div><!-- /gn-scroller -->
+		</nav>
+
+	</li><!-- /gn-trigger -->
+
+	<li>
+		<?php print render($easy_admin['easy_admin_home']); ?>
+	</li>
+
+	<li>
+    	<?php print render($easy_admin['easy_admin_user']); ?>
+    </li>
+
+    <li>
+    	<?php print render($easy_admin['easy_admin_menu']); ?>
+    </li>
+
+</ul>
+</div>
