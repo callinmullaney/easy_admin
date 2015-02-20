5a3679b6f919a88b416133c8d03008c2df439ace
callinmullaney <callin@callinmullaney.me>
2015-02-20 17:05:01 -0600

    integrated JS for timer functions on hover. added wrapping elements in prep for the new version styling. added body class for page offset

---
+++ assets/js/easy_admin.js
@@ -0,0 +1,48 @@
+(function ($, Drupal) {
+
+  function myFunction () {
+    clearTimeout(myTimer);
+  }
+
+  function myStopFunction() {
+    myTimer = setTimeout(function () {
+      $('.easy-admin').removeClass('secondLevel');
+    }, 1000);
+  }
+
+  Drupal.behaviors.play = {
+    attach: function(context, settings) {
+
+    }
+  };
+
+  $(document).ready(function() {
+    $('.manager-menu > li > a,.manager-menu > li > span,ul.user > li > a').wrapInner('<span />');
+
+    // sub nav functionality
+    $('.manager-menu a,.manager-menu span.nolink').click(function(e) {
+      if ($(this).parent().find('ul').length) {
+        e.preventDefault();
+        $('.easy-admin').addClass('secondLevel');
+      } else {
+        e.preventDefault();
+        alert('no subnav');
+      }
+    });
+
+    var myTimer;
+
+
+
+    // mouse enter
+    $('.easy-admin').mouseenter(function() {
+      myFunction();
+    });
+
+    // mouse out
+    $('.easy-admin').mouseleave(function() {
+      myStopFunction();
+    });
+  });
+
+})(jQuery, Drupal);
